a0e0d27d9b4f0d3b9684d01315ece02e8d42d1aa
Yago rocha <yagodasilvarocha30@gmail.com>
2025-08-09 18:10:03 -0300

    Implementada funcao para adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById("amigo").value.trim();
+
+    if (nome === "") {
+        alert('Por favor, insira um nome');
+    } else {
+        amigos.push(nome);
+        document.getElementById("amigo").value = "";
+        
+    }
+}
