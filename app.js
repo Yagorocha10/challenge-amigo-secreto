2eeffcf2b7803b9e26745bdcde8adc2904e9ef44
Yago rocha <yagodasilvarocha30@gmail.com>
2025-08-09 18:52:21 -0300

    Implementa funcao para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ function adicionarAmigo() {
     } else {
         amigos.push(nome);
         document.getElementById("amigo").value = "";
-        
+        atualizarListaAmigos();
     }
+
+
 }
+
+function atualizarListaAmigos() {
+     let lista = document.getElementById("listaAmigos");  
+     lista.innerHTML = "";
+    
+     for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+     }
+}
+
+
